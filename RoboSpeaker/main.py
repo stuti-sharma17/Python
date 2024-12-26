9de1c3060c658163433edbb6ae7d9baefe80d13b
Stuti Sharma <stutis070@gmail.com>
2024-12-26 10:40:10 +0530

    Initial Commit

---
+++ RoboSpeaker/main.py
@@ -0,0 +1,15 @@
+import pyttsx3
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker version 2.o")
+    while True:
+        x = input("Enter what you want the system to speak: ")
+        if x=="q":
+            engine = pyttsx3.init()
+            engine.say("BYE-BYE Friend !!")
+            engine.runAndWait()
+            break
+        engine = pyttsx3.init()
+        engine.say(x)
+        engine.runAndWait()
+
